dbd279ce4cecd4922feadf90a3d03bd1a9dc3d2a
asteriskpro <mamadou.soumare@tolbico.com>
2024-01-11 13:03:56 +0000

    mon commit message pour test

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
       <div
         class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-        <h1 class="title-font sm:text-5xl text-3xl mb-4 font-bold text-gray-900">Begin your coding
-          <br class="hidden lg:inline-block">journey
+        <h1 class="title-font sm:text-5xl text-3xl mb-4 font-bold text-gray-900">Mouhamadou Ibn Abdoulahi SOUMARE
+          <br class="hidden lg:inline-block">VoIP  & devops Engineer 
         </h1>
         <p class="mb-8 leading-relaxed">We are a pan-African Climate-Agtech using new technologies to significantly contribute to food security in Africa through sustainable agriculture and improving the lives of small-scale producers. We are committed to preserving nature and its development by involving all stakeholders.</p>
         <div class="flex justify-center">
