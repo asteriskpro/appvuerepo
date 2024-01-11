3c62eabffb67bf1eddf1311350c8a4060dbb46d8
asteriskpro <mamadou.soumare@tolbico.com>
2024-01-11 10:22:31 +0000

    mon commit message pour test

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,16 @@
         <h1 class="title-font sm:text-5xl text-3xl mb-4 font-bold text-gray-900">Begin your coding
           <br class="hidden lg:inline-block">journey
         </h1>
-        <p class="mb-8 leading-relaxed">They claim that nothing is insurmountable to learn. Learning to program is also
-          not impossible. All we need is your dedication and time. Our platform will help you stay focused with lots of
-          our features and curriculum</p>
+        <p class="mb-8 leading-relaxed">We are a pan-African Climate-Agtech using new technologies to significantly contribute to food security in Africa through sustainable agriculture and improving the lives of small-scale producers. We are committed to preserving nature and its development by involving all stakeholders.</p>
         <div class="flex justify-center">
           <a class="inline-block py-4 px-10 free-btn text-md " href="#">
-            TRY FOR FREE  LAYE DIAW LE master 8 testing  
+            About Tolbi Discover our origins and meet the team behind Tolbi.  
           </a>
           <a class="inline-block py-4 px-10 bg-btn text-md " href="#">
             SIGN UP NOW
+          </a>
+          <a class="inline-block py-4 px-10 bg-btn text-md " href="#">
+            e-Tolbi gives you total control over your agricultural activities. Monitor the progress of your crops, stay informed about the status of your fields, and make informed decisions.
           </a>
         </div>
       </div>
